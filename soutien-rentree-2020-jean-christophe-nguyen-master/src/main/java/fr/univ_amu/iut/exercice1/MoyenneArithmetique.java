ed9f75c4d37892191c6d52962e067df3ea851210
nguyenj-c <nguyenjeanchristophe951@gmail.com>
2022-04-24 11:30:03 +0200

    small fix of soutien-rentree

--- soutien-rentree-2020-jean-christophe-nguyen-master/src/main/java/fr/univ_amu/iut/exercice1/MoyenneArithmetique.java
+++ soutien-rentree-2020-jean-christophe-nguyen-master/src/main/java/fr/univ_amu/iut/exercice1/MoyenneArithmetique.java
@@ -1,11 +1,11 @@
 package fr.univ_amu.iut.exercice1;
 
 public class MoyenneArithmetique {
+
     public double calculer(TableauDeNotes tableau){
         double decimal = 0;
         for(int i=0; i<tableau.size();++i){
-            decimal+=tableau.get(i);
-
+            decimal+=(double) tableau.get(i);
         }
         return decimal;
     }
--- soutien-rentree-2020-jean-christophe-nguyen-master/src/main/java/fr/univ_amu/iut/exercice1/Note.java
+++ soutien-rentree-2020-jean-christophe-nguyen-master/src/main/java/fr/univ_amu/iut/exercice1/Note.java
@@ -4,6 +4,7 @@ package fr.univ_amu.iut.exercice1;
 public class Note {
    private double note;
    private Matiere refmatiere;
+
     public Note(Matiere refmatiere, double note){
         this.refmatiere=refmatiere;
         this.note=note;
